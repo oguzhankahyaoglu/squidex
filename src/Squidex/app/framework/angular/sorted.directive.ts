86e2dd773b021f946ef11e2ff5b44f452ff23193
oguzhankahyaoglu <oguzhan.kahyaoglu@yahoo.com>
2019-10-21 00:36:49 +0300

    css theme

--- src/Squidex/app/framework/angular/sorted.directive.ts
+++ src/Squidex/app/framework/angular/sorted.directive.ts
@@ -1,59 +1,59 @@
-/*
- * Squidex Headless CMS
- *
- * @license
- * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
- */
+// /*
+//  * Squidex Headless CMS
+//  *
+//  * @license
+//  * Copyright (c) Squidex UG (haftungsbeschränkt). All rights reserved.
+//  */
 
-import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+// import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 
-import * as Sortable from 'sortablejs';
+// import * as Sortable from 'sortablejs';
 
-@Directive({
-    selector: '[sqxSortModel]'
-})
-export class SortedDirective implements OnDestroy, OnInit {
-    private sortable: Sortable.Ref;
+// @Directive({
+//     selector: '[sqxSortModel]'
+// })
+// export class SortedDirective implements OnDestroy, OnInit {
+//     private sortable: Sortable.Ref;
 
-    @Input()
-    public dragHandle = '.drag-handle';
+//     @Input()
+//     public dragHandle = '.drag-handle';
 
-    @Input('sqxSortModel')
-    public sortModel: ReadonlyArray<any>;
+//     @Input('sqxSortModel')
+//     public sortModel: ReadonlyArray<any>;
 
-    @Output('sqxSort')
-    public sort = new EventEmitter<ReadonlyArray<any>>();
+//     @Output('sqxSort')
+//     public sort = new EventEmitter<ReadonlyArray<any>>();
 
-    constructor(
-        private readonly elementRef: ElementRef
-    ) {
-    }
+//     constructor(
+//         private readonly elementRef: ElementRef
+//     ) {
+//     }
 
-    public ngOnDestroy() {
-        if (this.sortable) {
-            this.sortable.destroy();
-        }
-    }
+//     public ngOnDestroy() {
+//         if (this.sortable) {
+//             this.sortable.destroy();
+//         }
+//     }
 
-    public ngOnInit() {
-        this.sortable = Sortable.create(this.elementRef.nativeElement, {
-            sort: true,
-            animation: 150,
+//     public ngOnInit() {
+//         this.sortable = Sortable.create(this.elementRef.nativeElement, {
+//             sort: true,
+//             animation: 150,
 
-            onSort: (event: { oldIndex: number, newIndex: number }) => {
-                if (this.sortModel && event.newIndex !== event.oldIndex) {
-                    const newModel = [...this.sortModel];
+//             onSort: (event: { oldIndex: number, newIndex: number }) => {
+//                 if (this.sortModel && event.newIndex !== event.oldIndex) {
+//                     const newModel = [...this.sortModel];
 
-                    const item = this.sortModel[event.oldIndex];
+//                     const item = this.sortModel[event.oldIndex];
 
-                    newModel.splice(event.oldIndex, 1);
-                    newModel.splice(event.newIndex, 0, item);
+//                     newModel.splice(event.oldIndex, 1);
+//                     newModel.splice(event.newIndex, 0, item);
 
-                    this.sort.emit(newModel);
-                }
-            },
+//                     this.sort.emit(newModel);
+//                 }
+//             },
 
-            handle: this.dragHandle
-        });
-    }
-}
+//             handle: this.dragHandle
+//         });
+//     }
+// }
